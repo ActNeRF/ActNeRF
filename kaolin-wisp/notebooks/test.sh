d1eb85a85080b1a19655aea8b63589442e686ad9
Optimus Prime <akshat.gpt20801@gmail.com>
2024-07-07 17:08:03 +0530

    removing more unncessary files

--- kaolin-wisp/notebooks/test.sh
+++
@@ -1,7 +0,0 @@
-for iter in {1..1}
-do
-    echo "Iteration $iter"
-    python next_best_pose_range_new_aksh.py -i test/input_$iter.pkl -r /home/saptarshi/dev/CustomComposer/robot_range2_lc_0.04.pkl -p test/ransam500_out$iter.pkl -d test/ransam500_out$iter/ -a test/faltu >test/ransam500_out$iter.txt
-    # python next_best_pose_range_new_aksh.py -i test/input_$iter.pkl -r /home/saptarshi/dev/CustomComposer/robot_range2_lc_0.04.pkl -p test/ransam500_nomove_out$iter.pkl  -d test/ransam500_nomove_out$iter/ -a test/faltu -l 0 >test/ransam500_nomove_out$iter.txt
-done
-
